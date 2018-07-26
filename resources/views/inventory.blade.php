91268f0a86f5b8e85a78c6bb0492ddf223389873
Denzehl Dacuyan <siegfredacuyan@gmail.com>
2018-07-26 11:46:47 +0800

    updated inventory style

--- resources/views/inventory.blade.php
+++ resources/views/inventory.blade.php
@@ -6,45 +6,52 @@
 
 @section('content')
     <div class="container">
-        <div class="container custom">
-            @if(Auth::user())
-                <div style="padding-bottom: 10px; color: #fff !important;"><button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Add Items</button></div>
-            @endif
-            <table id="items" class="table table-striped">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Item</th>
-                        <th>Image</th>
-                        <th>Stock Qty</th>
-                        <th>Price</th>
-                        <th>Options</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($items as $item)
-                        <tr>
-                            <td>{{ $item->id }}</td>
-                            <td>{{ $item->name }}</td>
-                            <td>
-                                <img class="inventoryimage" src="{{ asset('images/item_pics/' . $item->image)}}" alt="Update Image"></img>
-                            </td>
-                            <td>{{ $item->stock }}</td>
-                            <td>{{ $item->retail_price }} php</td>
-                            <td>
-                                <div style="color: #fff !important">
-                                    <a href="#"  data-sold="{{ $item->sold }}" data-desc="{{ $item->description }}"data-sold="{{$item->sold}}" data-id="{{$item->id}}" data-original_price="{{$item->original_price}}" data-retail_price="{{$item->retail_price}}" data-stock="{{$item->stock}}" data-name="{{$item->name}}" data-toggle="modal" data-target="#showmoreinventory" class="btn btn-success">More</a>
-                                    @if(Auth::user())
-                                        <a href="#" data-desc="{{ $item->description }}"data-sold="{{$item->sold}}" data-id="{{$item->id}}" data-original_price="{{$item->original_price}}" data-retail_price="{{$item->retail_price}}" data-stock="{{$item->stock}}" data-name="{{$item->name}}" data-toggle="modal" data-target="#update-item" class="btn btn-primary">Update</a>
-                                        <a type="button" href="#" data-id="{{$item->id}}" class="btn btn-danger delete">Delete</a>
-                                    @endif
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-12" style="padding-top: 30px;">
+                <div class="card">
+                    <div class="card-header">Inventory</div>
+                    <div class="card-body">
+			    @if(Auth::user())
+				<div style="padding-bottom: 10px; color: #fff !important;"><button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Add Items</button></div>
+			    @endif
+			    <table id="items" class="table table-striped">
+				<thead>
+				    <tr>
+					<th>#</th>
+					<th>Item</th>
+					<th>Image</th>
+					<th>Stock Qty</th>
+					<th>Price</th>
+					<th>Options</th>
+				    </tr>
+				</thead>
+				<tbody>
+				    @foreach($items as $item)
+					<tr>
+					    <td>{{ $item->id }}</td>
+					    <td>{{ $item->name }}</td>
+					    <td>
+						<img class="inventoryimage" src="{{ asset('images/item_pics/' . $item->image)}}" alt="Update Image"></img>
+					    </td>
+					    <td>{{ $item->stock }}</td>
+					    <td>{{ $item->retail_price }} php</td>
+					    <td>
+						<div style="color: #fff !important">
+						    <a href="#"  data-sold="{{ $item->sold }}" data-desc="{{ $item->description }}"data-sold="{{$item->sold}}" data-id="{{$item->id}}" data-original_price="{{$item->original_price}}" data-retail_price="{{$item->retail_price}}" data-stock="{{$item->stock}}" data-name="{{$item->name}}" data-toggle="modal" data-target="#showmoreinventory" class="btn btn-success">More</a>
+						    @if(Auth::user())
+							<a href="#" data-desc="{{ $item->description }}"data-sold="{{$item->sold}}" data-id="{{$item->id}}" data-original_price="{{$item->original_price}}" data-retail_price="{{$item->retail_price}}" data-stock="{{$item->stock}}" data-name="{{$item->name}}" data-toggle="modal" data-target="#update-item" class="btn btn-primary">Update</a>
+							<a type="button" href="#" data-id="{{$item->id}}" class="btn btn-danger delete">Delete</a>
+						    @endif
+						</div>
+					    </td>
+					</tr>
+				    @endforeach
+				</tbody>
+			    </table>
+			</div>
+		  </div>
+            </div>
+       </div>
     </div>
     @include('modals.inventory.createitems')
     @include('modals.inventory.updateitems')
